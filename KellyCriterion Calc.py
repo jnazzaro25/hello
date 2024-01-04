4f20573d37c83cb177ff840ce49e77b2fb68e05f
jnazzaro25 <jonathan.nazzaro@smartrent.com>
2024-01-04 09:10:42 -0700

    Adding implied probablity from American Odds

--- KellyCriterion Calc.py
+++ KellyCriterion Calc.py
@@ -2,12 +2,22 @@
 b = input("Enter Coefficient:")
 p = input("Enter Probablity of Winning:")
 q = input("Enter Probablity of Losing:")
-
+odds = input("Enter Current Odds of Bet:")
 float(b)
 float(p)
 float(q)
-
+float(odds)
 Result = (((float(b)*float(p))-float(q))/float(b))
+
+
+
+if float(odds) > 0:
+    Probablity = 100/(odds+100)*100
+    
+else:
+    abs(odds)   
+    Probablity = odds/(odds+100)*100
+
+print("This is the Implied Probablity for these Odds" + odds)
+
 print(Result)
-
-
