73e180c724ab560ae842fedcf0ada61e15c4150a
jnazzaro25 <jonathan.nazzaro@smartrent.com>
2024-01-04 15:58:06 -0700

    Created OddsConverter

---
+++ OddsConverter.py
@@ -0,0 +1,9 @@
+## Calculator to take odds and convert it to probablity percentage
+a = float(input("What are the odds"))
+odds = 0
+if a < 0:
+    odds = (-a)/(-a+100)
+    print(odds)
+else:
+    odds = 100/(100+a)
+    print(odds)
