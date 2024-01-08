5034d0bdca533cf99ea1d44cdf3cd593f45d0601
Jonathan Nazzaro <jonathan.nazzaro@smartrent.com>
2024-01-08 14:44:05 -0700

    Small Changes

--- BasicAFLMetrics.py
+++ BasicAFLMetrics.py
@@ -1,14 +1,19 @@
-def DisposalsPerMinute(): ##Team Disposals average per minutes 
-    return DisposalsPerMinute
+def DisposalsPerGame(Disposals):
+    DisposalsPerGame = Disposals/80 ##Team Disposals average per minutes 
+    return DisposalsPerGame
 def DroppedMarks(): #An uncontested marking opp that is dropped, resulting in a contest at ground level. Does not include Clangers because they are inside the 50. 
     return DroppedMarks
-def ScorePerGame(): 
+def ScorePerGame(TotalScore):
+    ScorePerGame = TotalScore/80  
     return ScorePerGame
-def GoalsperGame(): 
+def GoalsperGame(Goals):
+    GoalsperGame = Goals/80  
     return GoalsperGame
-def BehindPerGame(): 
+def BehindPerGame(Behinds):
+    BehindPerGame = Behinds/80 
     return BehindPerGame
-def LeadMarkPerGame():
+def LeadMarkPerGame(LeadMarks):
+    LeadMarkPerGame = LeadMarks/80
     return LeadMarkPerGame
 def MissedGoalOppourtunities (ShotsAttempts,Behinds,Goals): #Goal Success Percentage 
     MissedGoalOppourtunities = Goals/(Goals + ShotsAttempts + Behinds)
--- NHLDataScraping.py
+++ NHLDataScraping.py
@@ -1,9 +1,7 @@
 from nhlpy import NHLClient
+from nhlpy import Teams
 
 client = NHLClient()
+golden_knights = Teams.Team(54)
+print(golden_knights.stats())
 
-Ari = client.schedule.get_schedule_by_team_by_month('ARI')
-##print(str(Ari))
-
-test = client.game_center.score_now()
-print(str(test))
