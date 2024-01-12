a7362f5ed5a8b0074316ff61e4d09f371fb53ea9
Jonathan Nazzaro <jonathan.nazzaro@smartrent.com>
2024-01-12 11:00:09 -0700

    rerunning commit

--- NHLAPIDataConnection.py
+++ NHLAPIDataConnection.py
@@ -3,8 +3,11 @@ import requests
 import json
 import pandas as pd 
 
+def ConnectionCheck(): ## Check Connection of the NHL API 
+    team_Url = 'https://api-web.nhle.com/v1/standings/now'
+    team_response =  requests.get(team_Url)
+    return team_response
 
-team_Url = 'https://api-web.nhle.com/v1/standings/now'
-team_response =  requests.get(team_Url)
-print(team_response)
 
+def PullSchedule(date):
+    team_Url = 
--- NHLDataScraping.py
+++
@@ -1,7 +0,0 @@
-from nhlpy import team
-from nhlpy import schedule
-
-
-golden_knights = team.Team(54)
-print(golden_knights.stats())
-
