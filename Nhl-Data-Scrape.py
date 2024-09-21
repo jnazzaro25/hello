de7657b2eb5ba388b51c8d6a8f16768ce9a6cd8a
jnazzaro25 <jonathan.nazzaro@smartrent.com>
2024-09-21 12:26:19 -0700

    New Version of the code

--- Nhl-Data-Scrape.py
+++ Nhl-Data-Scrape.py
@@ -1,7 +1,17 @@
+##Packages
 import pandas as pd 
 import datetime as dt 
 from random import randint 
 from time import sleep 
 import os 
 
+## Variables
+url = "https://www.hockey-reference.com/leagues/NHL_2022_games.html"
+dfs = pd.read_html(url)
+df = dfs[0]
+dates = pd.to_datetime(df['Date'],format="ISO8601").dt.date
+dates = pd.Series(dates).drop_duplicates().tolist()
 
+##Code 
+print(dates)
+ 
